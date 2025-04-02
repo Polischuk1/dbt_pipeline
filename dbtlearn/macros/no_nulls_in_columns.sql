d17fd038804c9b9530624d4264e1e6c85ed7bdd8
Olha Polishchuk <92740388+Polischuk1@users.noreply.github.com>
2025-04-02 15:56:59 +0300

    Delete dbtlearn directory

--- dbtlearn/macros/no_nulls_in_columns.sql
+++
@@ -1,7 +0,0 @@
-{% macro no_nulls_in_columns(model)%}
-    select * from {{ model }} where 
-    {% for column in adapter.get_columns_in_relation(model) -%}
-        {{ column.name }} is null or 
-    {% endfor %}
-    false 
-{% endmacro %}
--- dbtlearn/macros/positive_value.sql
+++
@@ -1,4 +0,0 @@
-{% test positive_value(model,column_name)%}
-select * from {{model}} 
-where 1 > {{column_name}}
-{% endtest %}
